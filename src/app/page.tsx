f12a1ce7731ca678c33bad67b57809f7febc353b
KISMAT1234 <kismatbohora123@gmail.com>
2024-07-12 20:37:12 +0545

    skills and hero component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import Main from "@/components/Home/page";
 import Center from "@/components/Center/page";
 import Projects from "@/components/projects/page";
+import Hero from "@/components/Hero/page";
 import Image from "next/image";
+import Skills from "@/components/Skills/page";
 
 
 
@@ -9,6 +11,8 @@ export default function Home() {
   return (
    <>
     <Main/>
+    <Hero/>
+    <Skills/>
     <Center/>
     <Projects/>
    </>
